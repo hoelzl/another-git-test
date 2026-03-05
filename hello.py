9dd42684d109ed9069f442910e22b13c89016015
Matthias Hölzl <tc@xantira.com>
2026-03-05 11:26:26 +0100

    Refactor main execution block

--- hello.py
+++ hello.py
@@ -2,5 +2,7 @@ def say_hello(name):
     return f"Hi, {name}!"
 
 if __name__ == "__main__":
-    greeting = say_hello("World")
-    print(greeting)
+    print(say_hello("World"))
+    print(say_hello("Alice"))
+    print(say_hello("Bob"))
+    print(say_hello("Charlie"))
