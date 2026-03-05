30f8938b65e952d5b5efa74b1710941e1731682f
Matthias Hölzl <tc@xantira.com>
2026-03-05 10:58:40 +0100

    Add more example outputs

--- fact.py
+++ fact.py
@@ -7,3 +7,5 @@ def fact(n):
 if __name__ == "__main__":
     print(fact(5))  # Output: 120    print(fact(0))  # Output: 1
     print(fact(10)) # Output: 3628800
+    print(fact(15)) # Output: 1307674368000
+    print(fact(20)) # Output: 2432902008176640000
